9597a2aced799e04fb0aaf2ea5a309f54eef07b3
Yadav669 <tion3256@gmail.com>
2023-12-18 14:14:05 +0530

    ok

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Navbar from './components/Navbar'
-import TextForm from './components/TextForm';
+// import TextForm from './components/TextForm';
+import About from './components/About';
 
 function App() {
   // const h1="TextUtiles";
@@ -12,8 +13,10 @@ function App() {
          title="TextUtils" textabout="About Us" texthome="Home"
       />
       <div className="container my-3">
-        <TextForm heading="Enter Text Here"/>
+        {/* <TextForm heading="Enter Text Here"/> */}
+        <About/>
       </div>
+      
     </>
   );
 }
--- my-app/src/components/TextForm.js
+++ my-app/src/components/TextForm.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 
 export default  function TextForm(props){
+    
     const [text, setText] = useState('');
 
     const handleOnChange = (event) =>{
@@ -17,13 +18,19 @@ export default  function TextForm(props){
         setText(newText)
     }
 
+    const handleOnClearClick = () =>{
+        let newText = '';
+        setText(newText)
+    }
+
     return(
             <>
                 <div className="container">
                     <div class="mb-3">
                         <textarea className="form-control" id="myBox" value={text} onChange={handleOnChange} rows="8"></textarea>
-                        <button className="btn btn-primary my-2" onClick={handleOnClick}>To Uppercase</button>
+                        <button className="btn btn-primary my-2 mx-2" onClick={handleOnClick}>To Uppercase</button>
                         <button className="btn btn-primary my-2 mx-2" onClick={handleOnlClick}>To Lowercase</button>
+                        <button className="btn btn-primary my-2 mx-2" onClick={handleOnClearClick}>Clear Text</button>
                     </div>
                 </div>
 
@@ -32,7 +39,7 @@ export default  function TextForm(props){
                         <p>{text.split(" ").length} and {text.length} Characters </p>
                         <p>{0.008 * text.split(" ").length}Time to Read</p>
                     </div>
-                    <h4>Preview</h4>
+                    <h4><b>Preview</b></h4>
                     <p>{text}</p>
                 
             </>
